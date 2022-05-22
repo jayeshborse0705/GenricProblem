4e357ca980a357bc1b1853cc7c9a5f3caea2e9d0
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-05-22 17:03:57 +0530

    [jayesh] Extend max method

--- GenericProblem/MaximumNumberCheck.cs
+++ GenericProblem/MaximumNumberCheck.cs
@@ -53,5 +53,28 @@ namespace GenericProblem
                 }
                 throw new Exception("firstNumber,SecondNumber,thirdNumber are same");
             }
+        public static string MaximumStringNumber(string firstvalue, string secondvalue, string thirdvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >0 ||
+                firstvalue.CompareTo(secondvalue) >= 0 && firstvalue.CompareTo(thirdvalue) > 0 ||
+                firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) >=0)
+            {
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >0 ||
+               secondvalue.CompareTo(firstvalue) >= 0 && secondvalue.CompareTo(thirdvalue) > 0 ||
+               secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) >=0)
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >0 ||
+               thirdvalue.CompareTo(firstvalue) >= 0 && thirdvalue.CompareTo(secondvalue) > 0 ||
+               thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) >=0)
+            {
+                return thirdvalue;
+            }
+            throw new Exception("firstNumber,SecondNumber,thirdNumber are same");
         }
+
+    }
 }
